6e7724eca804b26a60c225734e1e94a3f9ad287b
vinal <vinchau01@gmail.com>
2024-10-18 14:23:15 -0700

    hopefully this will fix it

--- backend/db/models/index.js
+++ backend/db/models/index.js
@@ -2,7 +2,7 @@
 const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
-const config = require('../config/database')[process.env.NODE_ENV || 'development'];
+const config = require('../../config/database')[process.env.NODE_ENV || 'development'];
 const db = {};
 
 const sequelize = new Sequelize(config.database, config.username, config.password, config);
--- backend/utils/auth.js
+++ backend/utils/auth.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken');
-const { jwtConfig } = require('../db/config');
+const { jwtConfig } = require('../config');
 const { User } = require('../db/models');
 
 const { secret, expiresIn } = jwtConfig;
