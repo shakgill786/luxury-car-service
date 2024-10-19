71b32843adb71689d7e6355b80aaf00dd6a42153
vinal <vinchau01@gmail.com>
2024-10-18 17:56:32 -0700

    lets pray it works

--- backend/psql-setup-script.js
+++ backend/psql-setup-script.js
@@ -1,4 +1,6 @@
 const { sequelize } = require('./db/models');
+const config = require('../config/database');  // Adjust path if needed
+
 
 sequelize.showAllSchemas({ logging: false }).then(async (data) => {
   if (!data.includes(process.env.SCHEMA)) {
