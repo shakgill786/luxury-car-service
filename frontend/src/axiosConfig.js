23a1f37f839f1a53fabf9795e495f4f064e0afb6
shakgill786 <sgillani1@gmail.com>
2024-10-22 06:56:39 -0400

    starting over

--- frontend/src/axiosConfig.js
+++
@@ -1,6 +0,0 @@
-import axios from 'axios';
-
-axios.defaults.baseURL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:5000';
-axios.defaults.withCredentials = true;
-
-export default axios;
