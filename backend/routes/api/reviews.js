53ce48b0165be111fbf787c878dfa0af4c2379f7
vinal <vinchau01@gmail.com>
2024-10-27 11:52:44 -0700

    hope

--- backend/routes/api/reviews.js
+++ backend/routes/api/reviews.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { requireAuth } = require('../../utils/auth');
-const { Review, ReviewImage } = require('../../db/models');
+const { Review, ReviewImage, SpotImage, Spot, User } = require('../../db/models');
 const { check } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validation');
 
