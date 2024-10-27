a661405b96ea971c078f8e5b968885b584a7b14e
vinal <vinchau01@gmail.com>
2024-10-27 10:08:40 -0700

    error handler added

--- backend/app.js
+++ backend/app.js
@@ -86,5 +86,16 @@ app.use((err, _req, _res, next) => {
   next(err);
 });
 
+app.use((err, _req, res, _next) => {
+  res.status(err.status || 500);
+  console.error(err);
+  res.json({
+    title: err.title || 'Server Error',
+    message: err.message,
+    errors: err.errors,
+    stack: isProduction ? null : err.stack
+  });
+});
+
 // Export the Express app
 module.exports = app;
